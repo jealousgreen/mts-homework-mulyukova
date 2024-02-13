e35aa627636ea871ebc5c1894be8d5ab734994f6
jealousgreen <KALI89_hot>
2024-02-13 18:32:35 +0300

    dev 2

--- src/java/ru/mtsbank/homework/Main.java
+++ src/java/ru/mtsbank/homework/Main.java
@@ -1,5 +1,8 @@
 package src.java.ru.mtsbank.homework;
 
 public class Main {
-    public static void main(String[] args){System.out.println("Hello world");}
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("===========");
+    }
 }
