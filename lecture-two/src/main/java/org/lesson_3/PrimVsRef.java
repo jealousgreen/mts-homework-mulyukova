6b3e39ca8e02a9cf69a273dc28f3824288b2b346
jealousgreen <KALI89_hot>
2024-02-20 15:41:51 +0300

    lesson 3

---
+++ lecture-two/src/main/java/org/lesson_3/PrimVsRef.java
@@ -0,0 +1,12 @@
+package lesson_3.variables;
+
+public  class PrimVsRef {
+
+    public static void main(String[] args){
+        byte bitePrim = 10; // примитивы
+        short shortPrim = 100;
+        int intPrim = 1000;
+        long longPrim = 100;
+
+    }
+}
--- src/java/ru/mtsbank/homework/Main.java
+++ src/java/ru/mtsbank/homework/Main.java
@@ -1,7 +1,12 @@
 package src.java.ru.mtsbank.homework;
 
 public class Main {
+    public void executeLineCode() {
+
+    }
     public static void main(String[] args) {
         System.out.println("Hello world");
+        int first = 10;
+        executeLineCode();
     }
 }
