7364ab38e119107f8b7c0af5790be089299f47fa
jealousgreen <KALI89_hot>
2024-02-20 15:15:33 +0300

    dev 2

--- src/java/ru/mtsbank/homework/Main.java
+++ src/java/ru/mtsbank/homework/Main.java
@@ -3,9 +3,5 @@ package src.java.ru.mtsbank.homework;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
-        DateTimeFormatter formatter =
-                DateTimeFormatter.ofPattern("HH:mm:ss MM/dd/yyyy");
-                System.out.println(now.format(formatter));
-
     }
 }
