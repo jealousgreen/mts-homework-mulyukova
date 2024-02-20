638abd41d1478db64ab064ac2a1d7541df66d7a2
jealousgreen <102552821+jealousgreen@users.noreply.github.com>
2024-02-20 19:58:55 +0300

    Update Main.java

--- src/java/ru/mtsbank/homework/Main.java
+++ src/java/ru/mtsbank/homework/Main.java
@@ -1,12 +1,8 @@
 package src.java.ru.mtsbank.homework;
 
 public class Main {
-    public void executeLineCode() {
-
-    }
     public static void main(String[] args) {
         System.out.println("Hello world");
         int first = 10;
-        executeLineCode();
     }
 }
